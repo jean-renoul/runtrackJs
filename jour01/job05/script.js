e192078dd794a86d6bd71e714d2f92ed1a1bfdd9
jean-renoul <jean.renoul@laplateforme.io>
2024-04-15 10:15:51 +0200

    ajout jobs 3, 4 et 5

---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+let joursSemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+
+function afficherJoursSemaine(){
+    for (let jour of joursSemaines){
+        console.log (jour)
+    }
+}
+afficherJoursSemaine()
