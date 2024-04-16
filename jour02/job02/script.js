c79875065297ba66a8b4ddd637de615ef717edb6
jean-renoul <jean.renoul@laplateforme.io>
2024-04-16 09:46:04 +0200

    job02 fini

---
+++ jour02/job02/script.js
@@ -0,0 +1,8 @@
+function showhide(){
+    let text = document.getElementById("text");
+    if (text.style.display == "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
